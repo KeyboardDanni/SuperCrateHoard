b7198eec17a62f080a9fce1021755dda07d2934f
Danni <34800072+KeyboardDanni@users.noreply.github.com>
2023-08-14 19:48:20 -0400

    Bitmap text rendering

---
+++ src/engine/text.ts
@@ -0,0 +1,172 @@
+import { Picture, Renderer } from "./graphics";
+
+const CODEPOINT_LINE_FEED = 10;
+const CODEPOINT_CARRIAGE_RETURN = 13;
+const CODEPOINT_SPACE = 32;
+const CODEPOINT_QUESTION_MARK = 63;
+
+class CursorState {
+    x: number;
+    y: number;
+    readonly originalX: number;
+    readonly lineHeight: number;
+    readonly wrapWidth: number;
+
+    constructor(x: number, y: number, wrapWidth: number, font: BMFont) {
+        this.x = x;
+        this.y = y;
+        this.originalX = x;
+        this.lineHeight = font.lineHeight;
+        this.wrapWidth = wrapWidth;
+    }
+
+    advance(char: BMFontChar) {
+        this.x += char.xAdvance;
+    }
+
+    newLine() {
+        this.x = this.originalX;
+        this.y += this.lineHeight;
+    }
+}
+
+export class BMFontChar {
+    x: number = 0;
+    y: number = 0;
+    w: number = 0;
+    h: number = 0;
+    xOffset: number = 0;
+    yOffset: number = 0;
+    xAdvance: number = 0;
+    page: number = 0;
+
+    constructor(init?: Partial<BMFontChar>) {
+        Object.assign(this, init);
+    }
+}
+
+export class BMFont {
+    readonly pages: Picture[];
+    readonly chars: { [id: number]: BMFontChar } = {};
+    readonly lineHeight: number;
+
+    // Takes a bmfont2json-generated descriptor
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor(descriptor: any) {
+        this.lineHeight = descriptor.common.lineHeight;
+        this.pages = descriptor.pages.map((path: string) => new Picture(path));
+
+        if (this.pages.length <= 0) {
+            throw new Error("No texture pages defined in font descriptor");
+        }
+
+        for (const char of descriptor.chars) {
+            this.chars[char.id] = new BMFontChar({
+                x: char.x,
+                y: char.y,
+                w: char.width,
+                h: char.height,
+                xOffset: char.xoffset,
+                yOffset: char.yoffset,
+                xAdvance: char.xadvance,
+                page: char.page,
+            });
+        }
+    }
+
+    private getChar(id: number) {
+        return (
+            this.chars[id] ??
+            this.chars[CODEPOINT_QUESTION_MARK] ??
+            new BMFontChar()
+        );
+    }
+
+    private readChar(id: number, cursor: CursorState) {
+        switch (id) {
+            case CODEPOINT_LINE_FEED:
+                cursor.newLine();
+                return null;
+            case CODEPOINT_CARRIAGE_RETURN:
+                return null;
+        }
+
+        const char = this.getChar(id);
+
+        if (
+            cursor.x + char.xOffset + char.w >
+            cursor.originalX + cursor.wrapWidth
+        ) {
+            cursor.newLine();
+        }
+
+        return char;
+    }
+
+    private printChar(
+        char: BMFontChar,
+        cursor: CursorState,
+        renderer: Renderer
+    ) {
+        const picture = this.pages[char.page];
+
+        renderer.drawSprite(
+            picture,
+            {
+                x: char.x,
+                y: char.y,
+                w: char.w,
+                h: char.h,
+            },
+            cursor.x + char.xOffset,
+            cursor.y + char.yOffset
+        );
+    }
+
+    private advanceChar(char: BMFontChar, cursor: CursorState) {
+        cursor.x += char.xAdvance;
+    }
+
+    drawText(
+        renderer: Renderer,
+        text: string,
+        x: number,
+        y: number,
+        wrapWidth: number = Infinity
+    ) {
+        const cursor = new CursorState(x, y, wrapWidth, this);
+        const words = text.split(" ");
+        const space = this.getChar(CODEPOINT_SPACE);
+
+        for (const word of words) {
+            const slices = Array.from(word);
+            let readahead = cursor.x;
+
+            // Start by checking word length to see if we need to wordwrap
+            for (const slice of slices) {
+                const id = slice.codePointAt(0) ?? 0;
+                const char = this.getChar(id);
+
+                if (readahead + char.xOffset + char.w > x + wrapWidth) {
+                    cursor.newLine();
+                    break;
+                }
+
+                readahead += char.xAdvance;
+            }
+
+            // Actually print the characters
+            for (const slice of slices) {
+                const id = slice.codePointAt(0) ?? 0;
+                const char = this.readChar(id, cursor);
+
+                if (char) {
+                    this.printChar(char, cursor, renderer);
+                    cursor.advance(char);
+                }
+            }
+
+            cursor.advance(space);
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Picture, PictureSlice } from "./engine/graphics";
 import { Gameloop } from "./engine/gameloop";
 import { DrawLogic, Scene, TickLogic } from "./engine/scene";
 import { lerp } from "./engine/util";
+import { BMFont } from "./engine/text";
+import * as fontDescriptor from "./res/Pixel12x10.json";
 
 const TEST_SLICE: PictureSlice = {
     x: 0,
@@ -13,8 +15,10 @@ const TEST_SLICE: PictureSlice = {
 class TestLogic implements TickLogic, DrawLogic {
     private tickCount = 0;
     private picture;
+    private font: BMFont;
 
     constructor() {
+        this.font = new BMFont(fontDescriptor);
         this.picture = new Picture("res/GameAtlas.png");
         Picture.waitForLoad();
     }
@@ -23,14 +27,27 @@ class TestLogic implements TickLogic, DrawLogic {
         this.tickCount += 1;
     }
     draw(gameloop: Gameloop, _scene: Scene, lerpTime: number) {
-        gameloop.renderer().context().imageSmoothingEnabled = false;
+        const renderer = gameloop.renderer();
+
+        renderer.context().imageSmoothingEnabled = false;
 
         const x = 64 + (this.tickCount % 60) * 4;
         const xPrev = 64 + ((this.tickCount - 1) % 60) * 4;
 
-        gameloop
-            .renderer()
-            .drawSprite(this.picture, TEST_SLICE, lerp(xPrev, x, lerpTime), 64);
+        renderer.drawSprite(
+            this.picture,
+            TEST_SLICE,
+            lerp(xPrev, x, lerpTime),
+            64
+        );
+
+        this.font.drawText(
+            renderer,
+            "Hello, world! This is some wrapped text!\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            256,
+            256,
+            512
+        );
     }
 }
 
