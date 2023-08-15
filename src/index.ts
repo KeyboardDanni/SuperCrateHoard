bc13ecb37357ac18e56254c517460e6b19249f49
Danni <34800072+KeyboardDanni@users.noreply.github.com>
2023-08-15 16:50:33 -0400

    Outline font

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { DrawLogic, Scene, TickLogic } from "./engine/scene";
 import { lerp } from "./engine/util";
 import { BMFont } from "./engine/text";
 import * as fontDescriptor from "./res/Pixel12x10.json";
+import * as outlineFontDescriptor from "./res/Pixel12x10_Outline.json";
 
 const TEST_SLICE: PictureSlice = {
     x: 0,
@@ -16,9 +17,11 @@ class TestLogic implements TickLogic, DrawLogic {
     private tickCount = 0;
     private picture;
     private font: BMFont;
+    private outlineFont: BMFont;
 
     constructor() {
         this.font = new BMFont(fontDescriptor);
+        this.outlineFont = new BMFont(outlineFontDescriptor);
         this.picture = new Picture("res/GameAtlas.png");
         Picture.waitForLoad();
     }
@@ -41,7 +44,9 @@ class TestLogic implements TickLogic, DrawLogic {
             64
         );
 
-        this.font.drawText(
+        const font = this.tickCount % 120 >= 60 ? this.outlineFont : this.font;
+
+        font.drawText(
             renderer,
             "Hello, world! This is some wrapped text!\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             256,
